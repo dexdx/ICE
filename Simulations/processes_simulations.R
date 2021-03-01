276ca91f2a35bbfb56083607aa2892c6e91dba83
dexdx <74004141+dexdx@users.noreply.github.com>
2021-03-01 21:13:14 +0100

    import coefs

--- Simulations/processes_simulations.R
+++ Simulations/processes_simulations.R
@@ -17,7 +17,50 @@ library(MTS)
 library(urca)
 library(Matrix)
 library(matlib)
+library(lpSolve)
+library(glmnet)
+library(MASS)
 
 M = 100
+K = 50
 
 set.seed(3005)
+
+
+##########################################################################
+#### 1) VAR(1) sample paths simulations ##################################
+##########################################################################
+
+#' Import coefficients tables if on different session than the one
+#' coefs_simulations.R' was run on
+#' & restructure the tables as lists of lists
+A.full = as.matrix(read.table('YOUR PATH/A.full.txt'))
+B.full = as.matrix(read.table('YOUR PATH/B.full.txt'))
+
+S = 4
+A.list = vector(mode = 'list', length = S)
+for(l in 1:S){
+  A.list[[l]] = vector(mode = 'list', length = M)
+  for(m in 1:M){
+    A.list[[l]][[m]] = A.full[((l-1)*K*M + K*(m-1) + 1):((l-1)*K*M + K*m),]
+  }
+}
+
+B.list = vector(mode = 'list', length = S)
+for(l in 1:S){
+  B.list[[l]] = B.full[,((l-1)*M+1):(l*M)]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
