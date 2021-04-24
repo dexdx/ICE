9409a2cbec40ea8866f18ec176f07adef764c757
dexdx <74004141+dexdx@users.noreply.github.com>
2021-04-24 17:16:23 +0200

    sDiCE

--- Simulations/fit_simulations.R
+++ Simulations/fit_simulations.R
@@ -35,7 +35,7 @@ X.list = list(X1.list,X2.list,X3.list,X4.list)
 B.list = list(B1.full,B2.full,B3.full,B4.full)
 
 
-##### 1 ) LASSO ##########################################################
+##### 1) LASSO ###########################################################
 
 # initliaze performance matrix of LASSO
 L1B.lasso = matrix(0,M,length(X.list))
@@ -119,7 +119,7 @@ for(l in 1:length(X.list)){
 
 
 
-##### 1 ) sYWE ###########################################################
+##### 2) sYWE ############################################################
 L1B.sYWE = matrix(0,M,length(X.list))
 
 
@@ -176,14 +176,114 @@ for(l in 1:length(X.list)){
 }
 
 
+##### 3) sYWE ############################################################
+L1B.sPSC = matrix(0,M,length(X.list))
+
+for(l in 1:length(X.list)){
+  for(m in 1:M){
+    
+    start = proc.time()[3]
+    end = start + 600
+    done = FALSE
+    
+    while(proc.time()[3] < end && done == FALSE){
+      
+      x = X.list[[l]][[m]]
+      N = dim(x)[1]
+      K = dim(x)[2]
+      x.train = x[1:(N/2),]
+      x.test  = x[(N/2+1):N,]
+      
+      A.tune = sPSC(x.train)
+      
+      num.tune = length(A.tune)
+      x.fitted.tune = vector(mode = 'list', length = num.tune)
+      resid.tune = vector(mode = 'list', length = num.tune)
+      ssr.tune = numeric(num.tune)
+      
+      for(u in 1:num.tune){
+        
+        x.fitted.tune[[u]] = VAR1.fit(x.test[1,], nrow(x.test), A.tune[[u]])
+        resid.tune[[u]] = x.test - x.fitted.tune[[u]]
+        ssr.tune = sapply(resid.tune, function(x) sum(x^2))
+        ssr.ord = order(ssr.tune)
+        
+        if(ssr.ord[1] == 1){
+          s = 2
+          while(ssr.ord[s] == ssr.ord[s-1] + 1 && s < num.tune) s = s + 1
+          tune.min = ssr.ord[s]
+        } else tune.min = which.min(ssr.tune)
+        
+      }
+      
+      A = A.tune[[tune.min]]
+      PI = A - diag(1,K)
+      B = ICE(PI, 1)
+
+      L1B.sPSC[m,l] = sum((B.list[[l]][,m] - B)^2)
+      write.table(L1B.sPSC, file = 'Output/performance results/L1B.sPSC.txt')
+      
+      done = TRUE
+      
+    }
+  }
+}
 
 
+##### 4) DiCE ############################################################
+L1B.dice = matrix(0,M,length(X.list))
+
+for(l in 1:length(X.list)){
+  for(m in 1:M){
+    
+    x = X.list[[l]][[m]]
+    N = dim(x)[1]
+    K = dim(x)[2]
+    # x.train = x[1:(N/2),]
+    # x.test  = x[(N/2+1):N,]
+    
+    dice = ca.jo(x, type='trace', ecdet='none', spec='transitory')
+    B = dice@V[,1]
+    L1B.dice[m,l] = sum((B.list[[l]][,m] - B)^2)
+    write.table(L1B.dice, file = 'Output/performance results/L1B.dice.txt')
+    
+  }
+}
 
 
+##### 4) sDiCE ###########################################################
+L1B.sdice = matrix(0,M,length(X.list))
+source('Wilms_Functions.R')
 
-
-
-
+for(l in 1:length(X.list)){
+  for(m in 1:M){
+    
+    x = X.list[[l]][[m]]
+    x = x[1:floor(nrow(x)/2),]
+    
+    N = dim(x)[1]
+    K = dim(x)[2]
+    p = 2
+    r = 1
+    
+    dsY = embed(diff(x),p)
+    Z = x[p:(N-1),]
+    Y = dsY[,1:K]
+    X = dsY[,-(1:K)]
+    
+    dice = try(SparseCointegration_Lasso(Y=Y,X=X,Z=Z,beta.init=NULL,alpha.init=NULL,p=p,r=r,max.iter=10,conv=10^-2,lambda.gamma=matrix(seq(from=0.01,to=0.001,length=5),nrow=1),rho.glasso=seq(from=1,to=0.1,length=5),lambda_beta=matrix(seq(from=0.1,to=0.001,length=100),nrow=1)))
+    if(class(dice) == 'try-error'){
+      L1B.dice.mat[m,l] = NA
+      erate.dice.mat[m,l] = NA
+      write.table(L1B.dice.mat, file = 'Output/performance results/L1B.sDICE.txt')
+    } else{
+      B = dice$BETAhat
+      L1B.dice.mat[m,l] = sum((B.list[[l]][,m] - B)^2)
+      write.table(L1B.dice.mat, file = 'Output/performance results/L1B.sDICE.txt')
+    }
+    
+  }
+}
 
 
 
